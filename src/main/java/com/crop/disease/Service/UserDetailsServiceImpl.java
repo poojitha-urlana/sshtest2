e48172314bca77e941ac3d6681b0d1f40162bb2f
Poojitha <220101120194@cutm.ac.in>
2024-09-17 07:49:41 +0530

    commit

---
+++ src/main/java/com/crop/disease/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.crop.disease.Service;
+
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.crop.disease.Model.User;
+import com.crop.disease.Repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), (Collection<? extends GrantedAuthority>) user.getRoles());
+    }
+}
